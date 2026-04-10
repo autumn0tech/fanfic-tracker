43449b16ad5ee1866fc59076ef9d48ed76fa40a8
autumn0tech <autumntech1123@gmail.com>
2026-04-10 12:38:14 -0400

    feat: register favAuthors router

--- artifacts/api-server/src/routes/index.ts
+++ artifacts/api-server/src/routes/index.ts
@@ -1,10 +1,12 @@
 import { Router, type IRouter } from "express";
 import healthRouter from "./health";
 import ficsRouter from "./fics";
+import favAuthorsRouter from "./favAuthors";
 
 const router: IRouter = Router();
 
 router.use(healthRouter);
 router.use(ficsRouter);
+router.use(favAuthorsRouter);
 
 export default router;
